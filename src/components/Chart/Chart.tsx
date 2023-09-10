e58bde8f54a61af2878cec357334596d0acd96c8
jihye yu <dbwlgp1yng@gmail.com>
2023-09-10 17:10:08 +0900

    chore: separate chart components

---
+++ src/components/Chart/Chart.tsx
@@ -0,0 +1,45 @@
+import {
+  Area,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  CartesianGrid,
+  ComposedChart,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+import useChartData from "../../hooks/useChartData";
+import CustomToolTip from "../CustomToolTip/CustomToolTip";
+
+const Chart = () => {
+  const { data } = useChartData();
+
+  return (
+    <ResponsiveContainer width="100%" height={500} >
+      <ComposedChart
+        data={data}
+        margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
+        barGap={10}
+      >
+        <CartesianGrid stroke="#efefef" />
+        <XAxis dataKey="time" />
+        <YAxis
+          label={{ value: 'area', angle: -90, position: 'insideLeft' }}
+          yAxisId="left"
+        />
+        <YAxis
+          label={{ value: 'bar', angle: 90, position: 'insideRight' }}
+          yAxisId="right"
+          orientation="right"
+        />
+        <Tooltip content={<CustomToolTip />} />
+        <Legend />
+        <Area dataKey="value_area" fill="#ff7300" stroke="#ff7300" yAxisId="left" />
+        <Bar dataKey="value_bar" barSize={20} fill="#7ac4c0" yAxisId="right" />
+      </ComposedChart>
+    </ResponsiveContainer>
+  )
+};
+
+export default Chart;
